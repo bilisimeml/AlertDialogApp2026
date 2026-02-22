c0963b510ee18bf4f5d0db8352722168ed2add73
Akif Korkmaz <akifkorkmaztr@gmail.com>
2026-02-22 09:39:53 +0300

    dialogAc fonksiyonu eklendi

--- app/src/main/java/com/example/alertdialogapp/MainActivity.kt
+++ app/src/main/java/com/example/alertdialogapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.alertdialogapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +22,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun dialogAc(view : View){
+
     }
 }
